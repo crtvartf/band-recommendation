b038bb68caa535b374c8dfb2ebe675275374cc67
crtvartf <90901024+crtvartf@users.noreply.github.com>
2022-04-23 19:32:41 +0000

    Created and linked new contract Added input form and allowed it to write on a blockchain

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,47 @@ import useLocalStorage from 'use-local-storage';
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
-  /**
-   * Create a varaible here that holds the contract address after you deploy!
-   */
-  const contractAddress = "0xddFef2C227214730e82aB97802C2Be066F4cd3e9";
+
+  // Create a varaible here that holds the contract address after you deploy!
+  
+  const contractAddress = "0xa0DAeB94dFa7AeFA91aa52259b2D8066B74DF256";
   const contractABI = abi.abi;
+  const [allBands, setAllBands] = useState([]);
+
+  // Creating a method to extract all recommendations from blockchain
+
+  const getAllBands = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider
+        const signer = provider.getSigner();
+        const bandContract = new ethers.Contract(contractAddress, BandRecommendation.abi, signer);
+
+        // Calling "getAllBands" method from smart contract
+        
+        const bands = await bandContract.getAllBands();
+
+        // We need only clean data (address, readable timestamp and message)
+        
+        let bandsCleared = [];
+        bands.forEach(recommend => {
+          bandsCleared.push({
+            address: recommend.sender,
+            timestamp: new Date(recommend.timestamp * 1000),
+            message: recommend.message
+          });
+        });
+
+        // Storing required data in React State
+        
+        setAllBands(bandsCleared);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   
   const checkIfWalletIsConnected = async () => {
     try {
@@ -30,6 +66,7 @@ const App = () => {
         const account = accounts[0];
         console.log("Found an authorized account:", account);
         setCurrentAccount(account)
+        // await getAllBands();
       } else {
         console.log("No authorized account found")
       }
@@ -51,11 +88,18 @@ const App = () => {
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+      // await getAllBands();
     } catch (error) {
       console.log(error)
     }
   }
 
+  const [bandName, setBandName] = useState("");
+  
+  const handleChange = (event) => {
+    setBandName(event.target.value);
+  }
+  
   const recommend = async () => {
     try {
       const { ethereum } = window;
@@ -68,7 +112,9 @@ const App = () => {
         let count = await bandContract.getTotalRecommendations();
         console.log("Retrieved total number of recommendations...", count.toNumber());
 
-        const bandTxn = await bandContract.recommend();
+        // Getting the band name and writing it on a blockchain
+
+        const bandTxn = await bandContract.recommend(bandName);
         console.log("Mining...", bandTxn.hash);
 
         await bandTxn.wait();
@@ -96,18 +142,29 @@ const App = () => {
   }
   
   return (
+    
     <div className="app" data-theme={theme}>
+      <header>
+        
+      </header>
       <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
+        <div className="greetings">
+        🤘 Hey there!
         </div>
 
         <div className="container">
           Music makes our life better. Name a band that you think deserves to be remembered.
         </div>
+
+        <input
+          type="text"
+          placeholder="Band name"
+          onChange={handleChange}
+          required 
+        />
         
         <button onClick={recommend}>
-          Name a band
+          Recommend
         </button>
 
         {!currentAccount && (
@@ -115,11 +172,13 @@ const App = () => {
             Connect Wallet
           </button>
         )}
+        
       </div>
       <div className="theme-toggle">
-        <h2>Light Theme</h2>
+        <h2>Dark Theme</h2>
         <i onClick={switchTheme} className='fas fa-toggle-on'></i>
       </div>
+      
     </div>
   );
 }
